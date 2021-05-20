c69078566d355ca19f7f493a4d86165ca8bbf9ca
chaplygindenys <chaplygindenys@gmail.com>
2021-05-20 19:37:17 +0300

    add .env change .gitignore

--- db.js
+++ db.js
@@ -1,8 +1,16 @@
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({
+    path: path.join(__dirname, './.env')
+})
 const Sequelize = require('sequelize');
+
                                 //database username   password
-const sequelize = new Sequelize('gamedb', 'postgres', 'ghastb0i', {
-    host: 'localhost',
-    dialect: 'postgres'
+const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
 })
 
 sequelize.authenticate().then(
